cc19dd2a6a60f1223c84d944446a15fad678f4b4
clodiusnero <kristofer.mikaelsson@vuxutb.se>
2016-11-14 22:19:50 +0100

    update

---
+++ lab-2/lab-2/recepie.php
@@ -0,0 +1,11 @@
+﻿<html>
+<head>
+    <title>recepie</title>
+    <meta charset="utf-8" />
+</head>
+<body>
+
+    Thank you,<?php echo $_POST["Firstname"]; ?> <?php echo $_POST["LastName"]; ?><br>
+    Your email is: <?php echo $_POST["email"]; ?>
+</body>
+</html>
